c432b5548f36716c598eec0b58698f4f3715823c
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-10-11 10:25:51 +0900

    sqlを修正

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,41 +1,41 @@
 CREATE TABLE user (
     user_id INT PRIMARY KEY,
-    mailaddress VARCHAR(255),
-    password VARCHAR(255),
-    nickname VARCHAR(255),
-    icon_path VARCHAR(255),
-    skill_point INT,
-    rank_point INT,
-    hitpoint INT,
-    attack INT,
-    defence INT,
-    agility INT,
-    luck INT
+    mailaddress VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
+    icon_path VARCHAR(255) DEFAULT 'default_icon.png',
+    skill_point INT NOT NULL DEFAULT 1,
+    rank_point INT NOT NULL DEFAULT 1,
+    hitpoint INT NOT NULL DEFAULT 1,
+    attack INT NOT NULL DEFAULT 1,
+    defence INT NOT NULL DEFAULT 1,
+    agility INT NOT NULL DEFAULT 1,
+    luck INT NOT NULL DEFAULT 1
 );
 
 CREATE TABLE task (
     task_id INT PRIMARY KEY,
-    title VARCHAR(255),
-    detail TEXT,
-    period DATETIME,
-    is_complete TINYINT(1),
+    title VARCHAR(255) NOT NULL DEFAULT 'タイトル',
+    detail TEXT NOT NULL,
+    period DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_complete TINYINT(1) DEFAULT 0,
     completion_time DATETIME,
-    created_time DATETIME,
-    last_edit_time DATETIME,
-    user_id INT,
+    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_edit_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 CREATE TABLE match_table (
     match_id INT PRIMARY KEY,	
-    user_is_win TINYINT(1),
-    enemy_user_id INT,
-    user_id	INT,
+    user_is_win TINYINT(1) NOT NULL, 
+    enemy_user_id INT NOT NULL,
+    user_id	INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 CREATE TABLE skill (
     skill_id INT PRIMARY KEY,
-    skill_name VARCHAR(255),
-    skill_description TEXT
+    skill_name VARCHAR(255) NOT NULL,
+    skill_description TEXT NOT NULL
 );
