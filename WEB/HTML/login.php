21841bc8a4b49c1e9842da440287c1717e26db78
2101032 <2101032@s.asojuku.ac.jp>
2023-10-27 11:23:08 +0900

    a

--- WEB/HTML/login.php
+++ WEB/HTML/login.php
@@ -9,22 +9,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../CSS/login.css?<?php echo date('YmdHis'); ?>"/>
   <title>ログイン</title>
 </head>
 <body>
-
+<div class="icon">
+    <img src="../images/icon.png" alt="アイコン">
+    </div>
   <form action="./login_check.php" method="post">
     
-    <label for="">メールアドレス</label>
-    <input type="text" name="mailaddress" id="mailaddress">
+    <label for=""></label>
+    <input type="text" placeholder="メールアドレス" name="mailaddress" id="mailaddress">
     <br>
-    <label for="password">パスワード</label>
-    <input type="password" name="password" id="password">
+    <label for="password"></label>
+    <input type="password" placeholder="パスワード" name="password" id="password">
     <br>
     <button type="submit">ログイン</button>
     
   </form>
-  <a href="./signup.php">新規登録はこちら</a>
+    <button><a href="./signup.php">新規登録はこちら</a></button>
 </body>
 </html>
 <?php
