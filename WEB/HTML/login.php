4d5fe531220815cecac331e1ffe6ba2621d75002
2101032 <2101032@s.asojuku.ac.jp>
2023-10-27 12:26:31 +0900

    a

--- WEB/HTML/login.php
+++ WEB/HTML/login.php
@@ -9,6 +9,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="../CSS/login.css?<?php echo date('YmdHis'); ?>"/>
   <title>ログイン</title>
 </head>
@@ -16,18 +17,23 @@
 <div class="icon">
     <img src="../images/icon.png" alt="アイコン">
     </div>
+
   <form action="./login_check.php" method="post">
-    
-    <label for=""></label>
-    <input type="text" placeholder="メールアドレス" name="mailaddress" id="mailaddress">
+
+ <div class="textbox">
+    <input type="text"  placeholder="パスワード" style="width:35%;" name="mailaddress" >
+   <br><br>
+    <input type="password" placeholder="パスワード" style="width:35%;" name="password" id="password">
     <br>
-    <label for="password"></label>
-    <input type="password" placeholder="パスワード" name="password" id="password">
-    <br>
+</div>
+<div class="button">
     <button type="submit">ログイン</button>
-    
+    <br>
+    <br>
   </form>
     <button><a href="./signup.php">新規登録はこちら</a></button>
+    <br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
