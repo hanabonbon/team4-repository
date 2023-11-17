a1acd4f991715cad260bb601daa243d825f71468
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-11-17 16:36:43 +0900

    wip

---
+++ WEB/game/PlayerState.php
@@ -0,0 +1,21 @@
+<?php
+  class PlayerState {
+    private int $hihPoint;
+    private int $attack;
+    private int $defense;
+    private int $agility;
+    private int $luck;
+
+    public function __construct() {
+      $this->hihPoint = 0;
+      $this->attack = 0;
+      $this->defense = 0;
+      $this->agility = 0;
+      $this->luck = 0;
+    }
+
+    
+
+
+  }
+?>
