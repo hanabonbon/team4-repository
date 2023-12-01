6213a18489745309e5c5643f133bbf2320af20a8
hanabonbon <107159047+hanabonbon@users.noreply.github.com>
2023-12-01 11:55:52 +0900

    スキルポイント追加処理

--- WEB/DAO/Test.php
+++ WEB/DAO/Test.php
@@ -8,17 +8,17 @@
       $this->pdo = $connection->getPdo();
     }
 
-    //ランク順位
-    public function selectAllRanking(){
-        $sql = "SELECT * FROM user ORDER BY rank_point DESC";
-        $ps = $this->pdo->prepare($sql);
-        $ps->execute();
-        $result = $ps->fetchAll(PDO::FETCH_ASSOC);
+  //ランク順位
+  public function selectAllRanking(){
+    $sql = "SELECT * FROM user ORDER BY rank_point DESC";
+    $ps = $this->pdo->prepare($sql);
+    $ps->execute();
+    $result = $ps->fetchAll(PDO::FETCH_ASSOC);
 
-        return $result;
-    }
-    //自分のランク順位
-    public function selectMyRanking($user_id){
+    return $result;
+  }
+  //自分のランク順位
+  public function selectMyRanking($user_id){
     $sql = "SELECT user_id, rank_point,
           (SELECT COUNT(DISTINCT rank_point) FROM user WHERE rank_point > u.rank_point) + 1 AS rank
           FROM user u
@@ -33,6 +33,24 @@
     } else {
         echo "User not found or an error occurred.";
     }
+  }
+  public function getSpByUserId($user_id){
+    $sql = "SELECT skill_point FROM user WHERE user_id = ?";
+    $ps = $this->pdo->prepare($sql);
+    $ps->bindValue(1, $user_id, PDO::PARAM_INT);
+    $ps->execute();
+    $result = $ps->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+  }
+
+  //スキルポイント加算
+  public function updateSkillPoint($sp, $user_id){
+    $sql = "UPDATE user SET skill_point = ? WHERE user_id = ?";
+    $ps = $this->pdo->prepare($sql);
+    $ps->bindValue(1, $sp, PDO::PARAM_INT);
+    $ps->bindValue(2, $user_id, PDO::PARAM_INT);
+    $ps->execute();
   }
 }
 ?>
--- WEB/HTML/ranking.php
+++ WEB/HTML/ranking.php
@@ -30,42 +30,46 @@
   ?>
   <body style="background-color:#FFEED5;">
   <div class="container-fluid">
-        <div class="row">
-            <!-- サイドバー -->
-            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block  text-white sidebar  fixed-top">
-              <div class="position-sticky">
-                  <ul class="nav flex-column">
-                    <!--アイコンとユーザー名-->
-                    <div class="icon-name">
-                      <div class="img-area">
-                        <img src="../images/<?= $myuser['icon_path'] ?>" class="img-icon">
-                      </div>
-                      <div class="name-area">
-                        <label class="username-area"><?= $myuser['nickname'] ?></label>
-                      </div>
+      <div class="row">
+          <!-- サイドバー -->
+          <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block  text-white sidebar  fixed-top">
+            <div class="position-sticky">
+                <ul class="nav flex-column">
+                  <!--アイコンとユーザー名-->
+                  <div class="icon-name">
+                    <div class="img-area">
+                      <img src="../images/<?= $myuser['icon_path'] ?>" class="img-icon">
                     </div>
-  
-                      <li class="nav-item active">
-                        <!-- タスク上の白線 -->
-                        <div class="nav-link"></div>
-                          <a class="nav-link" href="task_list.php">タスク</a>
-                      </li>
-  
-                      <li class="nav-item">
-                          <a class="nav-link" href="mypage.php">マイページ</a>
-                      </li>
-  
-                      <li class="nav-item">
-                          <a class="nav-link" href="battle.html">対戦</a>
-                      </li>
-  
-                      <li class="nav-item">
-                        <a class="nav-link" href="ranking.php">ランキング</a>
-                      </li>
-                  </ul>
-          </nav>
+                    <div class="name-area">
+                      <label class="username-area"><?= $myuser['nickname'] ?></label>
+                    </div>
+                  </div>
+
+                    <li class="nav-item active">
+                      <!-- タスク上の白線 -->
+                      <div class="nav-link"></div>
+                        <a class="nav-link" href="home.php">ホーム</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="task_list.php">タスク</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="mypage.php">マイページ</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="battle.html">対戦</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link" href="ranking.php">ランキング</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-      </div>
+    </div>
       <!-- メニューバーここまで -->
     <h2 class="text-center mt-5">ランキング</h2>
     <h5 class="text-center mt-3">現在の順位：<?php $test->selectMyRanking($_SESSION['user_id']) ?>位</h5>
