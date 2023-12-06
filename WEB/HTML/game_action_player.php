b702b67935ac8f5739ff4c9afc2c4fcda4c24ee8
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-12-07 01:45:41 +0900

    wip: 攻撃処理もできた！

--- WEB/HTML/game_action_player.php
+++ WEB/HTML/game_action_player.php
@@ -23,20 +23,20 @@
   echo 'BattleController/userId:'. $battle->getPlayerId(). '<br>';
 
   //プレイヤーの行動
-  if(isset($_GET['attack]'])) {
+  if(isset($_GET['attack'])) {
     $battle->attack($_SESSION['opponentId']);
-    throw new \Exception('攻撃');
+    //throw new \Exception('攻撃');
 
   } else if(isset($_GET['defence'])) {
     $battle->defence($_SESSION['user_id']);
-    throw new \Exception('防御');
+    //throw new \Exception('防御');
 
   } else if(isset($_GET['avoid'])) {
     //未実装
     //$battle->avoid();
     throw new \Exception('回避');
   } else {
-    //throw new \Exception('行動分岐エラー');
+    throw new \Exception('行動分岐エラー');
   }
   
   //プレイヤー操作を無効化
